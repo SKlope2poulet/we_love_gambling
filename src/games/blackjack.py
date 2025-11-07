88a540d0991aa85cec436a738e4ba1d5de932f82
unknown <bfontaine@iut-lumiere.univ-lyon2.fr>
2025-11-07 16:24:17 +0100

    ✂️ US3 - Split de paire (création de 2 mains)

--- src/games/blackjack.py
+++ src/games/blackjack.py
@@ -8,3 +8,6 @@ class BlackjackGame:
         for bet in bets:
             self.balance -= bet
             self.hands.append({"cards": [], "bet": bet})
+    def split(self):
+        if len(self.player_hand) == 2 and self.player_hand[0][1] == self.player_hand[1][1]:
+            self.hands = [[self.player_hand[0]], [self.player_hand[1]]]
